f445c3e25b3c30d5e32bfe614f1efd4bc367a839
martyncolmer <martyc1974@hotmail.com>
2019-02-07 12:17:06 +0000

    Jenko

--- features/steps/update_account.py
+++ features/steps/update_account.py
@@ -5,6 +5,6 @@ UPDATE_ACCOUNT_URL = f'/accounts/%s'
 
 @when('I close the account')
 def get_account_by_context(context):
-    response = context.web_client.patch(UPDATE_ACCOUNT_URL % context.account_number
-                                        , json={"account_status": "closed"})
+    response = context.web_client.patch(UPDATE_ACCOUNT_URL % context.account_number,
+                                        json={"account_status": "closed"})
     context.response = response
